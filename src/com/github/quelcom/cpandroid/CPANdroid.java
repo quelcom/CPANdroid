3bd6b21800679c710358384ab908233c481b0979
Josep Roca <quelcom@gmail.com>
2011-09-26 18:35:39 +0300

    Initial commit

---
+++ src/com/github/quelcom/cpandroid/CPANdroid.java
@@ -0,0 +1,13 @@
+package com.github.quelcom.cpandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CPANdroid extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
